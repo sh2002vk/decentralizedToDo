8a0b22281d04f8e52bf9ef8f1cac4e285806ccde
sh2002vk <sh2002vk@gmail.com>
2022-04-02 21:12:05 -0700

    done contract

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool complete;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    constructor() public {
+        createTask("finish this todo app");
+    }
 }
